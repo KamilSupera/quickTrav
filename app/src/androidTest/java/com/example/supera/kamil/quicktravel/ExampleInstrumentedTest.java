51a3b91d7f7ea1b431c76b67615e40ee3f8f44a7
kaniak <kaniak274@gmail.com>
2019-02-15 17:21:42 +0100

    Bootstrap android project

---
+++ app/src/androidTest/java/com/example/supera/kamil/quicktravel/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.example.supera.kamil.quicktravel;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.supera.kamil.quicktravel", appContext.getPackageName());
+    }
+}
