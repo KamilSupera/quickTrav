1e765650a9d8172ed929e54babaf7e7edf2bf1f3
Kamil Kucharski <kaniak274@gmail.com>
2019-02-17 14:44:41 +0100

    Add splash screen class

--- app/src/main/java/com/example/supera/kamil/quicktravel/activities/MainActivity.java
+++ app/src/main/java/com/example/supera/kamil/quicktravel/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.supera.kamil.quicktravel;
+package com.example.supera.kamil.quicktravel.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.supera.kamil.quicktravel.R;
 
 public class MainActivity extends AppCompatActivity {
 
