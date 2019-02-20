433de5700478635dc876f43fd3917d04c0387324
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-02-20 08:08:41 +0100

    Add map to main activity

--- app/src/main/java/com/example/supera/kamil/quicktravel/activities/MainActivity.java
+++ app/src/main/java/com/example/supera/kamil/quicktravel/activities/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.supera.kamil.quicktravel.activities;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.supera.kamil.quicktravel.R;
+import com.example.supera.kamil.quicktravel.fragments.GoogleMapFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = this.getSupportFragmentManager();
+        Fragment map = new GoogleMapFragment();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_content, map);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
