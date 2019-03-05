5e6c87ca3c3fb53cb0ec8c737b4f0e7f4a81ff11
Kamil Kucharski <kaniak274@gmail.com>
2019-03-05 20:59:29 +0100

    Add loading city

---
+++ app/src/main/java/com/example/supera/kamil/quicktravel/gps_location/CityLookupFail.java
@@ -0,0 +1,5 @@
+package com.example.supera.kamil.quicktravel.gps_location;
+
+public class CityLookupFail extends Exception {
+    public CityLookupFail() { super(); }
+}
--- app/src/main/java/com/example/supera/kamil/quicktravel/gps_location/GPSLocation.java
+++ app/src/main/java/com/example/supera/kamil/quicktravel/gps_location/GPSLocation.java
@@ -4,12 +4,18 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class GPSLocation implements LocationListener {
@@ -22,6 +28,27 @@ public class GPSLocation implements LocationListener {
     public GPSLocation(Context context, Activity activity) {
         this.context = context;
         this.activity = activity;
+    }
+
+    /**
+     * Loads city name from current GPS position.
+     * @return Name of city of current phone position.
+     * @throws DeviceDisabled - When GPS is not working on device.
+     * @throws IOException - When couldn't read from latitude or longitude from current location.
+     * @throws CityLookupFail - When city was not found.
+     */
+    public String getCityFromCurrentLocation() throws DeviceDisabled, IOException, CityLookupFail {
+        LatLng currentLocation = this.getDeviceLocation();
+
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        List<Address> addresses = geocoder.getFromLocation(currentLocation.latitude,
+            currentLocation.longitude, 1);
+
+        if (addresses.size() > 0) {
+            return addresses.get(0).getLocality();
+        } else {
+            throw new CityLookupFail();
+        }
     }
 
     private boolean checkPermission() {
